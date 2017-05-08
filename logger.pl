f910f4e68fc08416cfe286f928483bbec07e989f
Andrew Basterfield <abasterfield@gmail.com>
2017-05-08 12:08:32 +0100

    Initial checkin

---
+++ logger.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use IPC::Open3;
+use IO::Select;
+use Symbol;
+
+$SIG{__DIE__} = \&Carp::croak;
+$SIG{__WARN__} = \&Carp::cluck;
+
+$|++;
+
+my $chld_in;
+my $chld_out;
+my $chld_err;
+$chld_err = gensym(); # need to create a symbol for stderr
+
+my $pid;
+$pid = open3($chld_in, $chld_out, $chld_err, @ARGV );
+
+my $fds = { $chld_out => 'STDOUT', $chld_err => 'STDERR' };
+
+my $fd_select = IO::Select->new();
+$fd_select->add(\*STDIN, $chld_out, $chld_err);
+
+my $okToRun = 1;
+while ($okToRun && (my @ready = $fd_select->can_read)) {
+  foreach my $fd (@ready) {
+    unless (my $line = <$fd>) {
+      $fd_select->remove($fd);
+      close($chld_in) unless $fds->{$fd};
+      delete $fds->{$fd};
+      if (!scalar keys %$fds) {
+        $okToRun = 0;
+        last;
+      }
+      
+    } elsif ($fds->{$fd}) {
+      chomp($line);
+      logger($pid, $fds->{$fd}, $line);
+    } else {
+      print $chld_in $line;
+    }
+  }
+}
+
+waitpid($pid,0);
+exit $? >> 8;
+
+sub logger {
+  my $pid = shift;
+  my $level = shift;
+  my @lines = @_;
+  foreach my $line (map { split /\n/, $_ } @lines) {
+    chomp($line);
+    my @t=localtime();
+    printf "%04d-%02d-%02d %02d:%02d:%02d [%s] %s: %s\n",$t[5]+1900,$t[4]+1,@t[3,2,1,0], $pid, $level, $line;
+  }
+}
